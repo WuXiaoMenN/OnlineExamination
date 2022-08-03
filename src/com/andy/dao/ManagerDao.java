bdb76efb14f3c64e22d876138b9e4ab122c49428
WuXiaoMenN <66507865+WuXiaoMenN@users.noreply.github.com>
2022-08-03 16:33:54 +0800

    Revert "init remote repository and push project to origin"
    This reverts commit 26d0cb236c6fc0b58f9e02bb9a03aca650e294b1.

--- src/com/andy/dao/ManagerDao.java
+++
@@ -1,8 +0,0 @@
-package com.andy.dao;
-
-import com.andy.entity.Manager;
-
-public interface ManagerDao {
-	public Manager getManagerById(String id);
-	public boolean modifyPassword(String id,String newPassword);
-}
--- src/com/andy/service/ManagerService.java
+++
@@ -1,9 +0,0 @@
-package com.andy.service;
-
-import com.andy.entity.Manager;
-
-public interface ManagerService {
-	public Manager getManagerById(String id);
-	public boolean login(Manager manager);
-	public boolean modifyPassword(String id,String oldPassword,String newPassword);
-}
